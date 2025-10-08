ef2cc890f554ba5cf8f5781557c3604f5368ff37
leonidas <leoniaguirrejacmax@gmail.com>
2025-10-07 20:50:10 -0600

    avance proyecto 3 CRUD

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+export class CreateUserDto {
+
+  @ApiProperty({ required: true, example: 'ruso_clutch' })
+  name: string;
+  @ApiProperty({ required: true, example: 'ruso@shein.com' })
+  email: string;
+  username: string;
+  @ApiProperty({ required: true, example: '123' })
+  password: string;
+  @ApiProperty({ required: true, description: 'Tenant ID' })
+  tenantId: number;
+}
+
+
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
